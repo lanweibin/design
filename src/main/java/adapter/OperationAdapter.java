ec9544f583a1aac5ff97e90763711f6547d5abfb
lanweibin <lanweibin0203@outlook.com>
2017-11-12 20:11:38 +0800

    Adapter

---
+++ src/main/java/adapter/OperationAdapter.java
@@ -0,0 +1,22 @@
+package adapter;
+
+//操作适配器：适配器
+
+public class OperationAdapter implements ScoreOperation {
+    private QuickSort sortObj;//定义适配者QuickSort对象
+    private BinarySearch searchObj;
+
+    public OperationAdapter(){
+        sortObj = new QuickSort();
+        searchObj = new BinarySearch();
+    }
+
+
+    public int[] sort(int[] array) {
+        return sortObj.quickSort(array);
+    }
+
+    public int search(int[] array, int key) {
+        return searchObj.binarySearch(array,key);
+    }
+}
---
+++ src/main/java/adapter/ScoreOperation.java
@@ -0,0 +1,9 @@
+package adapter;
+
+//抽象成绩操作类：目标接口
+
+public interface ScoreOperation {
+    public int[] sort(int array[]);
+
+    public int search(int array[], int key);
+}
